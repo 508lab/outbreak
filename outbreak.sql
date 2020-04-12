58d3e100480e6e4d75db136f9852bace206f15c1
schizobulia <2833324528@qq.com>
2020-04-12 15:09:36 +0800

    docs: 重新生成outbreak.sql

--- outbreak.sql
+++ outbreak.sql
@@ -1,210 +1,276 @@
--- MySQL dump 10.14  Distrib 5.5.64-MariaDB, for debian-linux-gnu (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 4.4.15.10
+-- https://www.phpmyadmin.net
 --
--- Host: localhost    Database: outbreak
--- ------------------------------------------------------
--- Server version	5.5.64-MariaDB-1ubuntu0.14.04.1
+-- Host: localhost
+-- Generation Time: 2020-04-12 15:08:19
+-- 服务器版本： 5.5.62-log
+-- PHP Version: 5.6.40
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- 数据库: `outbreak`
+-- Database: `outbreak`
 --
 CREATE DATABASE IF NOT EXISTS `outbreak` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `outbreak`;
 
+-- --------------------------------------------------------
+
 --
--- Table structure for table `admin`
+-- 表的结构 `admin`
 --
 
-DROP TABLE IF EXISTS `admin`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `admin` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `admin` (
+  `id` int(11) NOT NULL,
   `name` varchar(200) NOT NULL COMMENT '用户名',
   `password` varchar(12) NOT NULL COMMENT '密码',
-  `level` int(1) NOT NULL COMMENT '级别',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`),
-  UNIQUE KEY `name` (`name`)
+  `level` int(1) NOT NULL COMMENT '级别'
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='utf8_general_ci';
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `admin`
+-- 转存表中的数据 `admin`
 --
 
-LOCK TABLES `admin` WRITE;
-/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
-INSERT INTO `admin` VALUES (1,'admin','123456',0);
-/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `admin` (`id`, `name`, `password`, `level`) VALUES
+(1, 'admin', '123456', 0);
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `article`
+-- 表的结构 `article`
 --
 
-DROP TABLE IF EXISTS `article`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `article` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` char(250) NOT NULL,
-  `content` text NOT NULL,
+CREATE TABLE IF NOT EXISTS `article` (
+  `id` int(11) NOT NULL,
+  `title` char(250) NOT NULL COMMENT '标题',
+  `content` text NOT NULL COMMENT '内容',
   `time` date NOT NULL,
-  `tag` char(100) DEFAULT NULL,
+  `tag` char(100) DEFAULT NULL COMMENT '标签',
   `sid` int(11) NOT NULL,
-  `audit` int(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `sid` (`sid`),
-  CONSTRAINT `article_ibfk_1` FOREIGN KEY (`sid`) REFERENCES `students` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 COMMENT='utf8_general_ci';
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `audit` int(1) NOT NULL DEFAULT '0' COMMENT '审核'
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COMMENT='utf8_general_ci';
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `article`
+-- 表的结构 `comments`
 --
 
-LOCK TABLES `article` WRITE;
-/*!40000 ALTER TABLE `article` DISABLE KEYS */;
-INSERT INTO `article` VALUES (4,'测试文章','<p>这是测试文章</p><figure class=\"image\"><img src=\"/public/article/22/1585481921701user.png\"></figure>','2020-03-30','[\"0\",\"1\",\"3\"]',22,1),(5,'test','<p>123123123</p>','2020-03-29','[\"0\"]',22,1),(6,'2','<p>123123</p>','2020-03-29','[\"0\"]',22,2),(9,'5','<p>12312</p>','2020-03-29','[\"0\"]',22,0),(10,'5151241','<p>123123</p>','2020-03-29','[\"0\"]',22,0),(11,'123123','<p>124124124</p>','2020-03-29','[\"0\"]',22,0),(12,'12312','<p>312312</p>','2020-03-29','[\"0\"]',22,0),(13,'412412','<p>312312312</p>','2020-03-29','[\"0\"]',22,0),(14,'12312','<p>3123123</p>','2020-03-29','[\"0\"]',22,0),(15,'12312','<p>3123123</p>','2020-03-29','[\"0\"]',22,0),(16,'1231','<p>23123</p>','2020-03-29','[\"0\"]',22,0);
-/*!40000 ALTER TABLE `article` ENABLE KEYS */;
-UNLOCK TABLES;
+CREATE TABLE IF NOT EXISTS `comments` (
+  `id` int(11) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '评论的时间',
+  `sid` int(11) NOT NULL COMMENT '学生id',
+  `content` varchar(350) NOT NULL COMMENT '评论内容',
+  `aid` int(11) NOT NULL COMMENT '文章id'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `students`
+-- 表的结构 `students`
 --
 
-DROP TABLE IF EXISTS `students`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `students` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `students` (
+  `id` int(11) NOT NULL,
   `name` varchar(300) NOT NULL COMMENT '姓名',
   `sex` int(1) NOT NULL COMMENT '0:男 1:女',
-  `department` varchar(300) NOT NULL DEFAULT '1' COMMENT '所在系',
+  `department` varchar(300) NOT NULL COMMENT '所在系',
   `clas` varchar(250) NOT NULL COMMENT '所在班级',
-  `studentid` int(10) NOT NULL COMMENT '学号',
-  `password` varchar(300) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`),
-  UNIQUE KEY `studentid` (`studentid`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8 COMMENT='utf8_general_ci';
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `studentid` varchar(12) NOT NULL COMMENT '学号',
+  `password` varchar(300) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=363 DEFAULT CHARSET=utf8 COMMENT='utf8_general_ci';
 
 --
--- Dumping data for table `students`
+-- 转存表中的数据 `students`
 --
 
-LOCK TABLES `students` WRITE;
-/*!40000 ALTER TABLE `students` DISABLE KEYS */;
-INSERT INTO `students` VALUES (22,'test',1,'信息工程系','19物流信息技术班',1501373434,'c602e848d382024feb75f90b10425a86');
-/*!40000 ALTER TABLE `students` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `students` (`id`, `name`, `sex`, `department`, `clas`, `studentid`, `password`) VALUES
+(362, 'test', 1, '信息工程系', '19物流信息技术班', '1501373434', 'c602e848d382024feb75f90b10425a86');
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `teacher`
+-- 表的结构 `teacher`
 --
 
-DROP TABLE IF EXISTS `teacher`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `teacher` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `teacher` (
+  `id` int(11) NOT NULL,
   `name` varchar(300) NOT NULL DEFAULT '老师' COMMENT '姓名',
   `sex` int(1) NOT NULL DEFAULT '1' COMMENT '0:男 1:女',
   `department` varchar(300) NOT NULL DEFAULT '信息工程系' COMMENT '所在系',
-  `studentid` int(10) NOT NULL COMMENT '工号',
-  `password` varchar(300) NOT NULL DEFAULT 'c602e848d382024feb75f90b10425a86',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`),
-  UNIQUE KEY `studentid` (`studentid`)
-) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8 COMMENT='utf8_general_ci';
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `studentid` varchar(12) NOT NULL COMMENT '工号',
+  `password` varchar(300) NOT NULL DEFAULT 'c602e848d382024feb75f90b10425a86'
+) ENGINE=InnoDB AUTO_INCREMENT=1522 DEFAULT CHARSET=utf8 COMMENT='utf8_general_ci';
 
 --
--- Dumping data for table `teacher`
+-- 转存表中的数据 `teacher`
 --
 
-LOCK TABLES `teacher` WRITE;
-/*!40000 ALTER TABLE `teacher` DISABLE KEYS */;
-INSERT INTO `teacher` VALUES (28,'123456',1,'信息工程系',123456,'c602e848d382024feb75f90b10425a86');
-/*!40000 ALTER TABLE `teacher` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `teacher` (`id`, `name`, `sex`, `department`, `studentid`, `password`) VALUES
+(1521, 'test', 1, '信息工程系', '123456', 'c602e848d382024feb75f90b10425a86');
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `teachertemp`
+-- 表的结构 `teachertemp`
 --
 
-DROP TABLE IF EXISTS `teachertemp`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `teachertemp` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `teachertemp` (
+  `id` int(11) NOT NULL,
   `sid` int(11) NOT NULL COMMENT '老师Id',
   `record` float NOT NULL COMMENT '体温',
-  `time` date NOT NULL COMMENT '时间',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`),
-  KEY `studentid` (`sid`),
-  CONSTRAINT `teachertemp_ibfk_1` FOREIGN KEY (`sid`) REFERENCES `teacher` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `time` date NOT NULL COMMENT '时间'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `teachertemp`
+-- 表的结构 `temperature`
 --
 
-LOCK TABLES `teachertemp` WRITE;
-/*!40000 ALTER TABLE `teachertemp` DISABLE KEYS */;
-INSERT INTO `teachertemp` VALUES (19,28,37,'2020-03-31');
-/*!40000 ALTER TABLE `teachertemp` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `temperature`
---
-
-DROP TABLE IF EXISTS `temperature`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `temperature` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `temperature` (
+  `id` int(11) NOT NULL,
   `sid` int(11) NOT NULL COMMENT '用户Id',
   `record` float NOT NULL COMMENT '体温',
-  `time` date NOT NULL COMMENT '时间',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`),
-  KEY `studentid` (`sid`),
-  CONSTRAINT `temperature_ibfk_1` FOREIGN KEY (`sid`) REFERENCES `students` (`id`)
+  `time` date NOT NULL COMMENT '时间'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `temperature`
+-- Indexes for dumped tables
 --
 
-LOCK TABLES `temperature` WRITE;
-/*!40000 ALTER TABLE `temperature` DISABLE KEYS */;
-/*!40000 ALTER TABLE `temperature` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD UNIQUE KEY `name` (`name`);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indexes for table `article`
+--
+ALTER TABLE `article`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sid` (`sid`);
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD KEY `sid` (`sid`),
+  ADD KEY `aid` (`aid`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD UNIQUE KEY `studentid` (`studentid`);
+
+--
+-- Indexes for table `teacher`
+--
+ALTER TABLE `teacher`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD UNIQUE KEY `studentid` (`studentid`);
+
+--
+-- Indexes for table `teachertemp`
+--
+ALTER TABLE `teachertemp`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD KEY `studentid` (`sid`);
+
+--
+-- Indexes for table `temperature`
+--
+ALTER TABLE `temperature`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD KEY `studentid` (`sid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `article`
+--
+ALTER TABLE `article`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=363;
+--
+-- AUTO_INCREMENT for table `teacher`
+--
+ALTER TABLE `teacher`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1522;
+--
+-- AUTO_INCREMENT for table `teachertemp`
+--
+ALTER TABLE `teachertemp`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `temperature`
+--
+ALTER TABLE `temperature`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- 限制导出的表
+--
+
+--
+-- 限制表 `article`
+--
+ALTER TABLE `article`
+  ADD CONSTRAINT `article_ibfk_1` FOREIGN KEY (`sid`) REFERENCES `students` (`id`);
+
+--
+-- 限制表 `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`aid`) REFERENCES `article` (`id`),
+  ADD CONSTRAINT `studendcom` FOREIGN KEY (`sid`) REFERENCES `students` (`id`);
+
+--
+-- 限制表 `teachertemp`
+--
+ALTER TABLE `teachertemp`
+  ADD CONSTRAINT `teachertemp_ibfk_1` FOREIGN KEY (`sid`) REFERENCES `teacher` (`id`);
+
+--
+-- 限制表 `temperature`
+--
+ALTER TABLE `temperature`
+  ADD CONSTRAINT `temperature_ibfk_1` FOREIGN KEY (`sid`) REFERENCES `students` (`id`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2020-03-31 22:21:41
